c833227d9042ab981d1899e5b2d5324919b619a5
Alex Grant <alex@vermonster.com>
2015-06-22 16:59:04 -0400

    Be more strict with JSHint rules

--- client/tests/helpers/start-app.js
+++ client/tests/helpers/start-app.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
 import Application from '../../app';
-import Router from '../../router';
 import config from '../../config/environment';
 
 export default function startApp(attrs) {
-  var application;
+  let application;
 
-  var attributes = Ember.merge({}, config.APP);
-  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+  let attributes = Ember.merge({}, config.APP);
+  attributes = Ember.merge(attributes, attrs);
 
   Ember.run(function() {
     application = Application.create(attributes);
