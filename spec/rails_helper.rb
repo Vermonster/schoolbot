26d57a59ef12c9ce13b712f44f07f6f30cdd7607
Alex Grant <alex@vermonster.com>
2015-06-23 11:57:50 -0400

    Fix missing require for Poltergeist

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 
 require "rspec/rails"
 require "shoulda/matchers"
+require "capybara/poltergeist"
 
 Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |file| require file }
 
