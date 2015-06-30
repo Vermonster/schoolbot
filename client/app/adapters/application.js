43c278e81202b8ceaabb07b7e824aca0ff42378a
Alex Grant <alex@vermonster.com>
2015-06-30 16:11:35 -0400

    Silence deprecation warnings about shouldReloadAll

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,3 +1,8 @@
 import DS from 'ember-data';
 
-export default DS.ActiveModelAdapter.extend();
+export default DS.ActiveModelAdapter.extend({
+  // Always reload all models when `findAll` is used. This is currently the
+  // default behavior, but that will change in Ember 2.0, so not having the
+  // function defined gives deprecation warnings.
+  shouldReloadAll: () => true
+});
