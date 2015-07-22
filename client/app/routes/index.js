27e30dc67c10362a095ec70665ecf9be14234336
Beth Tenorio <betsky98@gmail.com>
2015-07-22 18:34:02 -0400

    FLAG for removal -  Make "current district" an injectable service
    This commit is already in master

--- client/app/routes/index.js
+++ client/app/routes/index.js
@@ -1,14 +1,12 @@
 import Ember from 'ember';
 import AuthenticatedRoute from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRoute, {
+export default Ember.Route.extend({
   districts: Ember.inject.service(),
 
   beforeModel: function() {
     if (!this.get('districts.current')) {
       this.transitionTo('about');
-    } else {
-      this._super(...arguments);
     }
   }
 });
