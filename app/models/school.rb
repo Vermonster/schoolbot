ae1c88ca6f37f970bce68d8bf94967ecd194d149
Alex Grant <alex@vermonster.com>
2015-07-23 17:15:34 -0400

    Enable full Rake to run on Travis-CI
    * Caches the gem bundle, node modules, and bower components
    * Uses the same version of Node we do (currently hardcoded)
    * Removes redundant RVM Ruby version since Travis can use .ruby-version
    * Adds Redis server to Travis environment
    * Allows geocoding to be skipped if lat/long are provided on new record
    
    Also removed some output redirection from bin/setup. This was intended to make
    the output cleaner for developers running the script, but for some reason this
    causes Travis to behave as though the command returned a successful exit code
    even if it didn't, and Bower would not be installed.

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,7 +2,13 @@ class School < ActiveRecord::Base
   belongs_to :district
 
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, unless: :skip_geocode?
 
   validates :name, :address, presence: true
+
+  private
+
+  def skip_geocode?
+    new_record? && latitude.present? && longitude.present?
+  end
 end
