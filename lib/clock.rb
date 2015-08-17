a8f6e8870ad67b8a83567994e75e569cbfa9064a
Alex Grant <alex@vermonster.com>
2015-08-17 14:08:13 -0400

    Add Airbrake notifications to clock process

--- lib/clock.rb
+++ lib/clock.rb
@@ -2,6 +2,10 @@ require File.expand_path('../../config/boot', __FILE__)
 require File.expand_path('../../config/environment', __FILE__)
 
 module Clockwork
+  error_handler do |error|
+    Airbrake.notify(error)
+  end
+
   every(20.seconds, 'UpdateBusLocations') do
     District.find_each do |district|
       UpdateBusLocationsJob.perform_later(district)
