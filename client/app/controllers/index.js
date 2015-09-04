6d46c16753ca457f6236bd19301bee6604e2bc7c
Alex Grant <alex@vermonster.com>
2015-09-04 15:16:38 -0400

    Sort students alphabetically in settings

--- client/app/controllers/index.js
+++ client/app/controllers/index.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   students: Ember.computed.filterBy('allStudents', 'isNew', false),
+  studentOrdering: ['nickname'],
+  orderedStudents: Ember.computed.sort('students', 'studentOrdering'),
 
   showingSettings: false,
   showingStudentForm: false,
