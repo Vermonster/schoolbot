ed272627fb97e98c6008e1db7a64e966dcab210a
Alex Grant <alex@vermonster.com>
2015-09-10 16:46:42 -0400

    Redirect the "www" subdomain to the root domain

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,4 +1,6 @@
 class ClientController < ActionController::Base
+  before_action :redirect_www
+
   def show
     render text: client_html
   end
@@ -7,5 +9,11 @@ class ClientController < ActionController::Base
 
   def client_html
     $redis.get($redis.get('client:current'))
+  end
+
+  def redirect_www
+    if request.subdomains.first == 'www'
+      redirect_to '//' + ENV.fetch('APPLICATION_HOST'), status: 301
+    end
   end
 end
