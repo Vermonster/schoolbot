2d2f3a997625d548b81ab353b4261df78bcb79f6
Fion Kou <fion@vermonster.com>
2015-10-05 13:49:27 -0400

    Add both `flash error` and `map` messages

--- client/app/controllers/map.js
+++ client/app/controllers/map.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  students: Ember.computed.filterBy('allStudents', 'isNew', false)
+  students: Ember.computed.filterBy('allStudents', 'isNew', false),
+
+  showingFlashMsg: true,
+  showingMapMsg: true,
+
+  actions: {
+    toggleFlashMsg() {
+      this.toggleProperty('showingFlashMsg');
+    }
+  }
 });
