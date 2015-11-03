0fc4f05d887bd785bdce7acd7141f5e6b05d8f59
Alex Grant <alex@vermonster.com>
2015-11-03 14:21:58 -0500

    Allow only lvh.me requests with WebMock instead of all

--- spec/support/puffing_billy.rb
+++ spec/support/puffing_billy.rb
@@ -2,11 +2,3 @@ Billy.configure do |config|
   config.whitelist += [/lvh\.me/]
   config.non_whitelisted_requests_disabled = true
 end
-
-RSpec.configure do |config|
-  config.around(:each, type: :feature) do |example|
-    WebMock.allow_net_connect!
-    example.run
-    WebMock.disable_net_connect!
-  end
-end
