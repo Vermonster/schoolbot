e05e0750f94fcaba62ce91a300753cf6563e828a
Alex Grant <grantovich@users.noreply.github.com>
2015-12-07 10:47:15 -0500

    Reset scroll on route transition for "page" routes

--- client/app/routes/register.js
+++ client/app/routes/register.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
+import ResetScrollMixin from './mixins/reset-scroll';
 import UnauthenticatedRouteMixin from
   'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin);
+export default Ember.Route.extend(ResetScrollMixin, UnauthenticatedRouteMixin);
--- client/app/routes/reset-password.js
+++ client/app/routes/reset-password.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
+import ResetScrollMixin from './mixins/reset-scroll';
 import UnauthenticatedRouteMixin from
   'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+export default Ember.Route.extend(ResetScrollMixin, UnauthenticatedRouteMixin, {
   model() {
     return this.store.createRecord('passwordReset');
   }
