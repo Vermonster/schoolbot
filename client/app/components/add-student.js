939609d2615af020876d5931c7b0de639fc3eaf4
Alex Grant <grantovich@users.noreply.github.com>
2015-12-08 17:38:03 -0500

    Use correct method to "cancel" new students
    `deleteRecord` is meant to be used on already-persisted records, and
    when called on a new record seems to incorrectly mark it as persisted,
    causing it to appear in the student list. As per the documentation,
    `rollbackAttributes` simply removes the record from the store if new.

--- client/app/components/add-student.js
+++ client/app/components/add-student.js
@@ -25,7 +25,7 @@ export default Ember.Component.extend({
     },
 
     cancel() {
-      this.get('student').deleteRecord();
+      this.get('student').rollbackAttributes();
       this.sendAction('canceled');
     }
   }
