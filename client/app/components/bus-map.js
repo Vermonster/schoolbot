e16273815ecc3fefc08239c6ec5bb1216876ac17
Alex Grant <grantovich@users.noreply.github.com>
2015-12-08 16:23:30 -0500

    Fix live tracking not disengaging when it should

--- client/app/components/bus-map.js
+++ client/app/components/bus-map.js
@@ -12,8 +12,13 @@ export default LeafletMap.extend({
 
   isTrackingBuses: true,
 
+  // FIXME: This is gross, but necessary since Leaflet offers no way to
+  // determine whether a zoom event was initiated by the user or the code
+  // https://github.com/Leaflet/Leaflet/issues/2934
+  nextZoomIsTracking: true,
+
   didCreateLayer() {
-    this.setTrackingBounds();
+    this._setTrackingBounds();
     this._super(...arguments);
   },
 
@@ -21,6 +26,14 @@ export default LeafletMap.extend({
   // https://github.com/miguelcobain/ember-leaflet/issues/11
   _dragstart() {
     this.set('isTrackingBuses', false);
+  },
+  _zoomstart() {
+    if (!this.get('nextZoomIsTracking')) {
+      this.set('isTrackingBuses', false);
+    }
+  },
+  _moveend() {
+    this.set('nextZoomIsTracking', false);
   },
 
   busLocationsChanged: Ember.observer('buses.@each.lastSeenAt', function() {
@@ -41,19 +54,13 @@ export default LeafletMap.extend({
       bounds.extend([bus.get('latitude'), bus.get('longitude')]);
     });
 
+    this.set('nextZoomIsTracking', true);
     this.set('bounds', bounds);
   },
 
   actions: {
-    zoomIn() {
-      this.set('isTrackingBuses', false);
-      this._layer.zoomIn();
-    },
-
-    zoomOut() {
-      this.set('isTrackingBuses', false);
-      this._layer.zoomOut();
-    },
+    zoomIn() { this._layer.zoomIn(); },
+    zoomOut() { this._layer.zoomOut(); },
 
     enableTracking() {
       this.set('isTrackingBuses', true);
