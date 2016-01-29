e009dc407d2cd9f0e707a83001be8cc0dfcab209
Alex Grant <grantovich@users.noreply.github.com>
2016-01-29 15:01:17 -0500

    Update main landing page for open-sourcing

--- client/app/components/main-landing.js
+++ client/app/components/main-landing.js
@@ -1,12 +1,6 @@
 import Ember from 'ember';
-import { request } from 'ic-ajax';
 
 export default Ember.Component.extend({
-  districtContactEmail: '',
-  districtContactInFlight: false,
-  districtContactSubmitted: false,
-  districtContactErrored: false,
-
   didInsertElement() {
     // Facebook button setup
     (function(d, s, id) {
@@ -29,31 +23,5 @@ export default Ember.Component.extend({
         fjs.parentNode.insertBefore(js, fjs);
       }
     }(document, 'script', 'twitter-wjs'));
-  },
-
-  actions: {
-    submitDistrictContact() {
-      this.set('districtContactErrored', false);
-
-      if (Ember.isPresent(this.get('districtContactEmail'))) {
-        this.set('districtContactInFlight', true);
-
-        request({
-          url: 'https://formspree.io/schoolbot-contact@vermonster.com',
-          method: 'POST',
-          dataType: 'json',
-          data: {
-            _replyto: this.get('districtContactEmail'),
-            _subject: 'District contact by ' + this.get('districtContactEmail')
-          }
-        }).then(() => {
-          this.set('districtContactSubmitted', true);
-        }).catch(() => {
-          this.set('districtContactErrored', true);
-        }).finally(() => {
-          this.set('districtContactInFlight', false);
-        });
-      }
-    }
   }
 });
