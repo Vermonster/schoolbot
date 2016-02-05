9ff73dcc0cbeedb6772246c6722276939cfd61d0
Alex Grant <grantovich@users.noreply.github.com>
2016-02-05 16:07:18 -0500

    Update Ember and frontend dependencies
    * New version of HTMLbars prohibits <script> tags in any templates,
      so this must be handled by didInsertElement
    
    * Ember 2.3 not installed yet because of deprecation spam (caused by
      ember-simple-auth not being updated to use the new getOwner API)
    
    * ember-cli-airbrake 0.1.0 not installed yet because it seems to cause
      PhantomJS crashes on the generic-error-handling feature specs

--- client/app/components/main-landing.js
+++ client/app/components/main-landing.js
@@ -7,6 +7,30 @@ export default Ember.Component.extend({
   districtContactSubmitted: false,
   districtContactErrored: false,
 
+  didInsertElement() {
+    // Facebook button setup
+    (function(d, s, id) {
+      let js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    // Twitter button setup
+    (function(d,s,id){
+      let js, fjs = d.getElementsByTagName(s)[0],
+        p = /^http:/.test(d.location) ? 'http' : 'https';
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + '://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    }(document, 'script', 'twitter-wjs'));
+  },
+
   actions: {
     submitDistrictContact() {
       this.set('districtContactErrored', false);
