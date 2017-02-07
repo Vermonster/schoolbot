fcfa2f011047a9ee674204a0ba6148832834408f
Dewar Tan <dewar@vermonster.com>
2017-02-07 16:05:36 -0500

    Simulate live-tracking of bus location

---
+++ app/jobs/simulate_bus_location_job.rb
@@ -0,0 +1,34 @@
+# rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+
+class SimulateBusLocationJob < ActiveJob::Base
+  DISTANCE = 1 / 3437.670013352 # radius_of_circle/earth_radius in miles
+
+  # These calculations simulate the bus driving around in a circle
+  def perform(iteration)
+    Bus.find_each do |bus|
+      degree = convert_to_radians iteration
+      bus_location = bus.bus_locations.first
+      unless bus_location.nil?
+        lat = convert_to_radians bus_location.latitude
+        long = convert_to_radians bus_location.longitude
+
+        new_latitude = convert_to_degrees(Math.asin(Math.sin(lat) * Math.cos(DISTANCE) + Math.cos(lat) * Math.sin(DISTANCE) * Math.cos(degree)))
+        new_longitude = convert_to_degrees(long + Math.atan2(Math.sin(degree) * Math.sin(DISTANCE) * Math.cos(lat), Math.cos(DISTANCE) - Math.sin(lat) * Math.sin(new_latitude)))
+
+        BusLocation.create(bus_id: bus.id, latitude: new_latitude,
+                           heading: bus_location.heading, longitude: new_longitude, recorded_at: Time.now.in_time_zone
+        )
+      end
+    end
+  end
+
+  private
+
+  def convert_to_radians(degrees)
+    degrees * Math::PI / 180
+  end
+
+  def convert_to_degrees(radians)
+    radians * 180 / Math::PI
+  end
+end
--- lib/clock.rb
+++ lib/clock.rb
@@ -6,14 +6,24 @@ module Clockwork
     Airbrake.notify(error)
   end
 
-  every(Zonar::REQUEST_INTERVAL, 'UpdateBusLocations') do
-    District.active.find_each do |district|
-      UpdateBusLocationsJob.perform_later(district)
+  unless ENV['ENABLE_BUS_LOCATION_SIMULATOR'].present?
+    every(Zonar::REQUEST_INTERVAL, 'UpdateBusLocations') do
+      District.active.find_each do |district|
+        UpdateBusLocationsJob.perform_later(district)
+      end
     end
   end
 
   every(1.day, 'CleanBusLocations') do
     BusLocation.where('created_at < ?', 1.week.ago).delete_all
+  end
+
+  if ENV['ENABLE_BUS_LOCATION_SIMULATOR'].present?
+    iteration = 0
+    every(Zonar::REQUEST_INTERVAL, 'SimulateBusLocation') do
+      SimulateBusLocationJob.perform_later(iteration)
+      iteration = iteration >= 360 ? 0 : iteration += 1 # reset circle
+    end
   end
 
   if INTERCOM_ENABLED
