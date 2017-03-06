2606a68990cfb630e74fda6fd0957b78263c3fda
Paul Wilson <paul@vermonster.com>
2017-03-06 16:29:56 -0500

    Center Icons

--- client/app/components/simple-marker.js
+++ client/app/components/simple-marker.js
@@ -3,6 +3,7 @@ import MarkerLayer from 'ember-leaflet/components/marker-layer';
 
 export default MarkerLayer.extend({
   iconClass: null,
+  iconSize: null,
   object: null,
 
   lat: Ember.computed.alias('object.latitude'),
@@ -10,7 +11,15 @@ export default MarkerLayer.extend({
   clickable: false,
   keyboard: false,
 
-  icon: Ember.computed('iconClass', function() {
-    return this.L.divIcon({ iconSize: null, className: this.get('iconClass') });
+  didInsertElement: function() {
+    this._super()
+    //Fix offset by Height and Width defined in Styles
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      var el = document.getElementsByClassName(this.get('iconClass'))[0]
+      this.set('iconSize', [el.clientHeight, el.clientWidth])
+    })
+  },
+  icon: Ember.computed('iconClass', 'iconSize', function() {
+    return this.L.divIcon({ iconSize: this.get('iconSize'), className: this.get('iconClass') });
   })
 });
