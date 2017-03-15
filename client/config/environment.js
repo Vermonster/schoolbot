7a03f0d1dee78681fbbf7dba88756587a2ff6bb2
Paul Wilson <paul@vermonster.com>
2017-03-15 17:07:42 -0400

    Add event tracking in intercom

--- client/config/environment.js
+++ client/config/environment.js
@@ -65,6 +65,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV['intercom'] = {
+      appId: null
+    }
     ENV['ember-cli-mirage'] = {
       enabled: true
     }
